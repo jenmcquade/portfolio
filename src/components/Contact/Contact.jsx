dcd3b9c98a69cafd721ed12a7c104e0d643a21a5
Jen McQuade <jenmcquade@gmail.com>
2020-11-12 11:01:46 -0800

    Add ReCAPTCHA npm package for contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,53 +1,60 @@
 import React, { useContext, useEffect } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import Fade from 'react-reveal/Fade';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
+const recaptchaRef = React.createRef();
+
 const Contact = () => {
   const { contact } = useContext(PortfolioContext);
-  const { cta, btn, email } = contact;
+  const { cta, btn } = contact;
+
+  let addContactFormActions = function() {
+    let form = document.querySelector('#contactForm');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      if (form.checkValidity() === false) {
+        event.stopPropagation();
+      } else {
+        recaptchaRef.current.execute();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  }
+
+  let onContactFormCaptchaApproval = (token) => {
+    const URL = "https://a81a99f6fa.execute-api.us-west-2.amazonaws.com/Prod/contact";
+    const name = document.querySelector('#contactName').value;
+    const email = document.querySelector('#contactEmail').value;
+    const message = document.querySelector('#contactMessage').value;
+    const data = {
+      name: name,
+      email: email,
+      message: message
+    };
+    fetch(URL, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then(() => {
+      let submitBtn = document.querySelector('#contactSubmit');
+      submitBtn.innerText = 'Thanks for your message!';
+      submitBtn.setAttribute('disabled', true);
+      submitBtn.classList.add('disabled');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
 
   useEffect(() => {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission if invalid
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        if (form.checkValidity() === false) {
-          event.stopPropagation();
-        } else {
-          const URL = "https://a81a99f6fa.execute-api.us-west-2.amazonaws.com/Prod/contact";
-          const name = document.querySelector('#contactName').value;
-          const email = document.querySelector('#contactEmail').value;
-          const message = document.querySelector('#contactMessage').value;
-          const data = {
-            name: name,
-            email: email,
-            message: message
-          };
-          fetch(URL, {
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(data)
-          })
-          .then(() => {
-            let submitBtn = document.querySelector('#contactSubmit');
-            submitBtn.innerText = 'Thanks for your message!';
-            submitBtn.setAttribute('disabled', true);
-            submitBtn.classList.add('disabled');
-          })
-          .catch(err => {
-            console.error(err);
-          });
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
+    addContactFormActions();
   }, []);
 
   return (
@@ -60,7 +67,7 @@ const Contact = () => {
               {cta || 'Would you like to work with me? Awesome!'}
             </p>
 
-            <form className="needs-validation" noValidate>
+            <form id="contactForm" className="needs-validation" noValidate>
               <div className="form-group">
                 <label htmlFor="contactName">Your Name</label>
                 <input type="text" className="form-control form-control-lg contact-input" id="contactName" placeholder="Your name" required />
@@ -82,18 +89,15 @@ const Contact = () => {
                   Please provide a message to send me.
                 </div>
               </div>
-              <button type="submit" id='contactSubmit' className="cta-btn cta-btn--resume g-recaptcha" data-sitekey="6LeD9eEZAAAAADAGDzn3tcg2DFy0db3md1onza_s" data-callback="validate">{btn}</button>
+              <button type="submit" id='contactSubmit' className="cta-btn cta-btn--resume">{btn}</button>
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                size="invisible"
+                data-badge="inline"
+                sitekey="6LeD9eEZAAAAADAGDzn3tcg2DFy0db3md1onza_s"
+                onChange={onContactFormCaptchaApproval}
+              />
             </form>
-            {/*
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-            >
-              {btn || "Let's Talk"}
-            </a>
-            */}
           </div>
         </Fade>
       </Container>
--- src/html.js
+++ src/html.js
@@ -19,8 +19,15 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         {headComponents}
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+
+              `,
+          }}
+        />
       </head>
       <body {...bodyAttributes}>
         {preBodyComponents}
