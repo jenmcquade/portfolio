0f630c4c32dccb68f4c3db39b7a6453a7ba12f49
Jen McQuade <jen.k.mcquade@gmail.com>
2021-04-06 09:14:27 -0700

    Added S3 Deployment configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,44 @@
+const path = require('path')
 module.exports = {
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
+        name: 'images',
+        path: path.join(__dirname, '/src/images/')
+      }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
+        name: 'pages',
+        path: path.join(__dirname, '/src/pages/')
+      }
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Jen McQuade, Web Developer`,
-        short_name: `JenMcQuade`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#02aab0`,
-        display: `standalone`,
-        icon: 'src/images/favicon.png',
-      },
+        name: 'Jen McQuade, Web Developer',
+        short_name: 'JenMcQuade',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#02aab0',
+        display: 'standalone',
+        icon: 'src/images/favicon.png'
+      }
     },
-  ],
-};
+    {
+      resolve: 'gatsby-plugin-s3',
+      options: {
+        bucketName: 'jmcquade.com',
+        protocol: 'https',
+        hostname: 'www.jmcquade.com'
+      }
+    }
+  ]
+}
